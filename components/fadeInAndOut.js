a1fd6f060c46d26ffa109703819dbe6fa913c2c2
Stoui <100451219+VictorLeblond@users.noreply.github.com>
2024-03-27 16:16:53 -0400

    we'll get it once

--- components/fadeInAndOut.js
+++ components/fadeInAndOut.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	
-	$("bodydiv").css("display", "none");
+	$("body").css("display", "none");
 
-    $("bodydiv").fadeIn(2000);
+    $("body").fadeIn(2000);
     
 	$("a.transition").click(function(event){
 		event.preventDefault();
 		linkLocation = this.href;
-		$("bodydiv").fadeOut(1000, redirectPage);		
+		$("body").fadeOut(1000, redirectPage);		
 	});
 		
 	function redirectPage() {
